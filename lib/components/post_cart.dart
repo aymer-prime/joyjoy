b8806c8456d4d58561e47ce072478e7b45ae88e5
Mostafa <mostafa@customsurgical.co>
2024-08-29 15:26:21 +0400

    add page builder to images

--- lib/components/post_cart.dart
+++ lib/components/post_cart.dart
@@ -26,7 +26,7 @@ class PostCart extends StatefulWidget {
   final VoidCallback onpress;
   final List<String> postText;
   bool userLiked;
-  final Media? mediainfo;
+  final List<Media>? mediainfo;
   PostCart(
       {super.key,
       required this.username,
@@ -174,19 +174,26 @@ class _PostCartState extends State<PostCart> {
             visible: (widget.mediainfo == null) ? false : true,
             child: (widget.mediainfo != null)
                 ? ClipRRect(
-                    child: (widget.mediainfo!.video == 0)
+                    child: (widget.mediainfo?[0].video == 0)
                         ? SizedBox(
-                            width: double.infinity,
-                            child: Image.network(
-                              widget.mediainfo!.src!,
-                              fit: BoxFit.cover,
-                            ),
-                          )
+                      width: double.infinity, // Maintains full width
+                      height: 450,
+                      child: PageView.builder(
+                        itemCount: widget.mediainfo!.length,
+                        itemBuilder: (context, index) {
+                          return Image.network(
+                            widget.mediainfo![index].src!,
+                            fit: BoxFit.cover, // Ensures the image covers the entire box
+                            width: double.infinity, // Ensures the image fills the width
+                          );
+                        },
+                      ),
+                    )
                         : SizedBox(
                             width: MediaQuery.of(context).size.width,
                             height: 450,
                             child: VideoPlayer(
-                              url: widget.mediainfo!.src!,
+                              url: widget.mediainfo?[0].src ?? "",
                               autoPlay: playderStatus,
                             ),
                           ),
